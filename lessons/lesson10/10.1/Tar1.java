056d7401b5def3c809cda7c3142094aa71ddf4a5
BHD1 <106514757+BHD1official@users.noreply.github.com>
2023-05-02 12:50:32 +0300

    base, java 10.1

--- lessons/lesson10/10.1/Tar1.java
+++ lessons/lesson10/10.1/Tar1.java
@@ -31,8 +31,57 @@ public class Tar1 {
 
   public static int Factorial(int n) {
     if (n > 1) {
-        return n * Factorial(n - 1);
+      return n * Factorial(n - 1);
     }
     return 1;
+  }
+
+  public static int a_n(int n) {
+
+    int a1 = 2;
+
+    if (n == 1) {
+      return a1;
+    }
+
+    int d = 3;
+    return a_n(n - 1) + d;
+  }
+
+  public static int fib(int n) {
+    if (n == 1 || n == 2) {
+      return 1;
+    }
+
+    int a_n1 = fib(n - 1);
+    int a_n2 = fib(n - 2);
+
+    return a_n1 + a_n2;
+  }
+
+  public static int sumElements(int n) {
+    int odd = 3;
+    int even = 5;
+
+    if (n == 0) {
+      return 0;
+    }
+
+    int S_n = sumElements(n-1); 
+    S_n += (n % 2 == 0) ? even : odd ;
+
+    return S_n;
+  }
+
+  public static int sumDigits(int n) {
+  
+    if (n == 0) {
+      return 0;
+    }
+
+    int s = n% 10;
+    s+= sumDigits(n/10); 
+
+    return s;
   }
 }
--- lessons/lesson10/10.1/Tester.java
+++ lessons/lesson10/10.1/Tester.java
@@ -1,6 +1,10 @@
 public class Tester {
     public static void main(String[] args) {
-        //Tar1.AllXDigitsNums(2);
-    System.out.println( Tar1.Factorial(4));
+        // Tar1.AllXDigitsNums(2);
+        System.out.println(Tar1.Factorial(4));
+        System.out.println(Tar1.a_n(3));
+        System.out.println(Tar1.fib(3));
+        System.out.println(Tar1.sumElements(5));
+        System.out.println(Tar1.sumDigits(123));
     }
 }
